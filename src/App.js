ae36c4e7db26479f6b1e8023f29809e2b4fb0a66
ciro9527 <ciro9527@gmail.com>
2019-10-30 23:52:44 +0800

    Revert "add material ui"
    This reverts commit 8481bad4993e05d5cdb46172435ef13f456f24f1.

--- src/App.js
+++ src/App.js
@@ -1,63 +1,25 @@
 import React from 'react';
-
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/core/styles';
-import { createMuiTheme } from '@material-ui/core/styles';
-import deepOrange from '@material-ui/core/colors/deepOrange';
-import orange from '@material-ui/core/colors/orange';
-import red from '@material-ui/core/colors/red';
+import logo from './logo.svg';
 import './App.css';
-
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-
-const theme = createMuiTheme({
-  palette: {
-    primary: deepOrange,
-    secondary: orange,
-    error: red,
-  },
-});
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <React.Fragment>
-        <CssBaseline />
-        <AppBar position="relative">
-          <Toolbar>
-            <Typography variant="h6" className="head-site-name">
-              解忧南杂店
-          </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-        <Container maxWidth="lg">
-          <Grid container spacing={2}>
-            <Grid item lg={2} md={6} sm={6} >
-              <Card>
-                <CardMedia component="img"
-                  alt="injoylife包包女包新款2019流行黑色斜挎包小包百搭ins复古单肩包"
-                  image="//img.alicdn.com/bao/uploaded/i4/2024444814/O1CN01vn5Nqa1lQrAhZY2mZ_!!0-item_pic.jpg"
-                  title="injoylife包包女包新款2019流行黑色斜挎包小包百搭ins复古单肩包" />
-                <CardContent>
-                  <Typography variant="h5" component="h2">
-                    injoylife包包女包新款2019流行黑色斜挎包小包百搭ins复古单肩包
-                </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          </Grid>
-        </Container>
-      </React.Fragment>
-    </ThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
