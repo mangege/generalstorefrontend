4ead27c82ee62e8ca45a3e0f5facaba5deeee059
ciro9527 <ciro9527@gmail.com>
2019-11-06 00:14:43 +0800

    add picture

--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -20,7 +20,13 @@ function Product() {
     return (
         <div className="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3 p-1">
             <div className="card shadow-sm">
-                <a href="#"><img src="https://gw.alicdn.com/bao/uploaded/O1CN01OCoixR1tVS68W5NOy_!!2-item_pic.png_240x240.jpg_.webp" className="card-img-top"></img></a>
+                <a href="#">
+                    <picture>
+                        <source srcSet="//img.alicdn.com/bao/uploaded/i4/2024444814/O1CN01vn5Nqa1lQrAhZY2mZ_!!0-item_pic.jpg_300x300_.webp" type="image/webp" />
+                        <source srcSet="//img.alicdn.com/bao/uploaded/i4/2024444814/O1CN01vn5Nqa1lQrAhZY2mZ_!!0-item_pic.jpg_300x300Q90.jpg" type="image/jpeg" />
+                        <img src="//img.alicdn.com/bao/uploaded/i4/2024444814/O1CN01vn5Nqa1lQrAhZY2mZ_!!0-item_pic.jpg_300x300Q90.jpg" className="card-img-top"></img>
+                    </picture>
+                </a>
                 <div className="card-body p-2 small">
                     <div className="mw-100 text-break text-truncate"><a className="text-dark" href="https://mo.m.taobao.com/union/daequan2c_2bd2b5f?pid=mm_0_0_0&show_tab=0">ANDORHEAL澜笛若慕 小棕瓶礼盒装6件套</a>
                     </div>
