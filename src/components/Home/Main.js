869e6623ebfa6dfc7d077ff2daaf76d3e7eb0358
ciro9527 <ciro9527@gmail.com>
2019-11-05 23:40:02 +0800

    update button sytle

--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -22,13 +22,11 @@ function Product() {
             <div className="card shadow-sm">
                 <a href="#"><img src="https://gw.alicdn.com/bao/uploaded/O1CN01OCoixR1tVS68W5NOy_!!2-item_pic.png_240x240.jpg_.webp" className="card-img-top"></img></a>
                 <div className="card-body p-2 small">
-                    <div class="mw-100 text-break text-truncate"><a className="text-dark" href="https://mo.m.taobao.com/union/daequan2c_2bd2b5f?pid=mm_0_0_0&show_tab=0">ANDORHEAL澜笛若慕 小棕瓶礼盒装6件套</a>
+                    <div className="mw-100 text-break text-truncate"><a className="text-dark" href="https://mo.m.taobao.com/union/daequan2c_2bd2b5f?pid=mm_0_0_0&show_tab=0">ANDORHEAL澜笛若慕 小棕瓶礼盒装6件套</a>
                     </div>
-                    <div className="text-dark d-flex justify-content-between"><span>¥<span className="font-weight-bold">10.0</span> <span class="text-muted">¥<del>20.0</del></span>
+                    <div className="text-dark d-flex justify-content-between"><span>¥<span className="font-weight-bold">10.0</span> <span className="text-muted">¥<del>20.0</del></span>
                     </span><span className="text-muted">已售 54321</span></div>
-                    <div className="text-center">
-                        <button className="btn btn-secondary btn-sm p-1">$pmC2YtmLdig$</button>
-                    </div>
+                    <button className="btn btn-secondary btn-sm w-100">$pmC2YtmLdig$</button>
                 </div>
             </div>
         </div>
