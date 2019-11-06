e629385ae4d7a946a3378f0e20186c73a013263b
ciro9527 <ciro9527@gmail.com>
2019-11-06 22:07:58 +0800

    无限滚动

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
         <header>
             <nav className="navbar navbar-dark bg-dark">
                 <div className="container d-flex justify-content-between">
-                    <a className="navbar-brand" href="#">解忧南杂店</a>
+                    <a className="navbar-brand" href="/">解忧南杂店</a>
                     <button type="button" className="btn btn-outline-secondary">登录</button>
                 </div>
             </nav>
--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -1,38 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import Axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function Nav() {
     return (
         <ul className="nav d-flex justify-content-between">
             <li className="nav-item ">
-                <a className="nav-link text-muted" href="#">大额券</a>
+                <a className="nav-link text-muted" href="/">大额券</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link text-muted" href="#">限量抢</a>
+                <a className="nav-link text-muted" href="/">限量抢</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link pr-0 text-muted" href="#">天猫国际精选</a>
+                <a className="nav-link pr-0 text-muted" href="/">天猫国际精选</a>
             </li>
         </ul>
     );
 };
 
-function Product() {
+function Product(props) {
+    let itemAttrs = props.item.attributes;
+    let jpgUrl = `${itemAttrs.pict_url}_300x300Q90.jpg`;
+    let webpUrl = `${itemAttrs.pict_url}_300x300_.webp`;
     return (
         <div className="col-6 col-sm-6 col-md-4 col-lg-3 col-xl-3 p-1">
             <div className="card shadow-sm">
-                <a href="#">
+                <a href={itemAttrs.referral_url}>
                     <picture>
-                        <source srcSet="//img.alicdn.com/bao/uploaded/i4/2024444814/O1CN01vn5Nqa1lQrAhZY2mZ_!!0-item_pic.jpg_300x300_.webp" type="image/webp" />
-                        <source srcSet="//img.alicdn.com/bao/uploaded/i4/2024444814/O1CN01vn5Nqa1lQrAhZY2mZ_!!0-item_pic.jpg_300x300Q90.jpg" type="image/jpeg" />
-                        <img src="//img.alicdn.com/bao/uploaded/i4/2024444814/O1CN01vn5Nqa1lQrAhZY2mZ_!!0-item_pic.jpg_300x300Q90.jpg" className="card-img-top"></img>
+                        <source srcSet={webpUrl} type="image/webp" />
+                        <source srcSet={jpgUrl} type="image/jpeg" />
+                        <img src={jpgUrl} className="card-img-top" alt={itemAttrs.title}></img>
                     </picture>
                 </a>
                 <div className="card-body p-2 small">
-                    <div className="mw-100 text-break text-truncate"><a className="text-dark" href="https://mo.m.taobao.com/union/daequan2c_2bd2b5f?pid=mm_0_0_0&show_tab=0">ANDORHEAL澜笛若慕 小棕瓶礼盒装6件套</a>
+                    <div className="mw-100 text-break text-truncate"><a className="text-dark" href={itemAttrs.referral_url}>{itemAttrs.title}</a>
                     </div>
-                    <div className="text-dark d-flex justify-content-between"><span>¥<span className="font-weight-bold">10.0</span> <span className="text-muted">¥<del>20.0</del></span>
-                    </span><span className="text-muted">已售 54321</span></div>
-                    <button className="btn btn-secondary btn-sm w-100">$pmC2YtmLdig$</button>
+                    <div className="text-dark d-flex justify-content-between"><span>¥<span className="font-weight-bold">{itemAttrs.price}</span> <span className="text-muted">¥<del>{itemAttrs.orig_price}</del></span>
+                    </span><span className="text-muted">已售 {itemAttrs.volume}</span></div>
+                    <button className="btn btn-secondary btn-sm w-100">{itemAttrs.referral_word}</button>
                 </div>
             </div>
         </div>
@@ -40,11 +46,26 @@ function Product() {
 }
 
 function ProductList() {
-    let items = Array(10).fill(1).map((a, i) => a + i);
+    const [items, setItems] = useState([]);
+    const [pageNo, setPageNo] = useState(0);
+    useEffect(() => {
+        Axios.get('http://192.168.2.181:3000/api/items').then(resp => {
+            setItems(i => [...i, ...resp.data.data]);
+        });
+    }, [pageNo]);
     return (
-        <div className="d-flex align-content-start flex-wrap py-1">
-            {items.map((item) => <Product key={item} />)}
-        </div>
+        <InfiniteScroll
+            dataLength={items.length}
+            next={() => { setPageNo(a => a + 1) }}
+            hasMore={true}
+            loader={<div className=" text-center"><div class="spinner-border"><p className="sr-only text-center">加载中...</p></div></div>}
+            endMessage={
+                <p className="text-center">没有了!</p>
+            }>
+            <div className="d-flex align-content-start flex-wrap py-1">
+                {items.map((item) => <Product key={item.id} item={item} />)}
+            </div>
+        </InfiniteScroll >
     );
 }
 
