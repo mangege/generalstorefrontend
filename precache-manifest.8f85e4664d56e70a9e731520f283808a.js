8b8c4d049f2b8513af657e822df5c4e8d2be4ebd
ciro9527 <ciro9527@gmail.com>
2019-11-08 02:47:14 +0800

    Updates

---
+++ precache-manifest.8f85e4664d56e70a9e731520f283808a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5152138d2ea8d500859072fb7d7ebf3e",
+    "url": "https://mangege.gitee.io/generalstorefrontend/index.html"
+  },
+  {
+    "revision": "934546223b10b8a29efe",
+    "url": "https://mangege.gitee.io/generalstorefrontend/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "16691f5f9972753e212f",
+    "url": "https://mangege.gitee.io/generalstorefrontend/static/css/main.4ea0c550.chunk.css"
+  },
+  {
+    "revision": "934546223b10b8a29efe",
+    "url": "https://mangege.gitee.io/generalstorefrontend/static/js/2.8fd47f07.chunk.js"
+  },
+  {
+    "revision": "16691f5f9972753e212f",
+    "url": "https://mangege.gitee.io/generalstorefrontend/static/js/main.a14ba30f.chunk.js"
+  },
+  {
+    "revision": "bd978893d15a86a37b1d",
+    "url": "https://mangege.gitee.io/generalstorefrontend/static/js/runtime-main.394065ef.js"
+  }
+]);
